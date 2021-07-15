26daea377141af825c3ddca57fd39e37b528baad
rui-min <minruiray@outlook.com>
2021-07-15 18:18:36 +0800

    Add an empty test class

--- src/main/java/Land.java
+++ src/main/java/Land.java
@@ -1,10 +1,12 @@
 import java.util.UUID;
+
 /**
  * Concrete class representing listed vacant land (level 2)
  */
 public class Land extends Property {
 
     /**
+     * Subclass constructor must call superclass's non-private constructor for inheritance.
      * @param uuid    the unique uuid of the property
      * @param address the unique address of the property, with unit No. if applicable
      * @param price   an initial price set to the listing property
@@ -13,6 +15,10 @@ public class Land extends Property {
         super(uuid, address, price);
     }
 
+    /**
+     * Override Object class's toString() method. Create a string representation of this class
+     * @return the string representation of this class
+     */
     @Override
     public String toString() {
         return "Land{" +
---
+++ src/test/java/LandTest.java
@@ -0,0 +1,36 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class LandTest {
+
+    @org.junit.jupiter.api.Test
+    void getUuid() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAddress() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setAddress() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPrice() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setPrice() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString1() {
+    }
+}
