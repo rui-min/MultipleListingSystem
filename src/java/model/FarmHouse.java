53da4aa6c16cbf2d031590118dc50e940fc90f57
rui-min <minruiray@outlook.com>
2021-07-20 09:03:52 +0800

    Gao's change done

--- src/java/model/FarmHouse.java
+++ src/java/model/FarmHouse.java
@@ -1,6 +1,7 @@
-package model; /**
+/**
  * Concrete class representing Farm House (level 4)
  */
+import java.time.LocalDate;
 import java.util.UUID;
 
 public class FarmHouse extends Freehold {
@@ -8,36 +9,118 @@ public class FarmHouse extends Freehold {
         super(uuid, address, price);
     }
 
-    /**
-     * An abstract method, which cannot be finished here and must be implemented
-     * by non-abstract subclass.
-     *
-     * @return the number of parking spaces.
-     */
-    @Override
-    public int getHowManyParks() {
-        return 0;
+    public static class Builder {
+        /**
+         * below are mandatory attributes
+         */
+        private UUID uuid;
+        private String address;
+        private int price;
+
+        /**
+         * below are optional attributes
+         */
+        private int nOfParkingSpace;
+        private String storageType;
+        private int nOfStorages;
+        private LocalDate builtDate;
+        private String type; // multi family or single family
+        private int numberOfStoreys;
+        private boolean isMultigeneration;
+
+        /**
+         * Builder constructor with three mandatory attributes: uuid, address, price
+         * @param uuid the unique uuid of the property
+         * @param address the unique address of the property
+         * @param price selling price of the listed property
+         */
+        public Builder(UUID uuid, String address, int price) {
+            this.uuid = uuid;
+            this.address = address;
+            this.price = price;
+        }
+
+        /**
+         * Set the unit number of the property
+         * @param nOfParkingSpace of the property
+         * @return the Builder object
+         */
+        public Builder withHowManyParks(int nOfParkingSpace){
+            this.nOfParkingSpace = nOfParkingSpace;
+            return this;
+        }
+
+        /**
+         * Set the number of parks of the property
+         * @param storageType of the property
+         * @return the Builder object
+         */
+        public Builder storageTypeIs(String storageType){
+            this.storageType = storageType;
+            return this;
+        }
+        /**
+         * Set the number of parks of the property
+         * @param type of the property
+         * @return the Builder object
+         */
+        public Builder typeIs(String type){
+            this.type = type;
+            return this;
+        }
+        /**
+         * Set the number of parks of the property
+         * @param builtDate of the property
+         * @return the Builder object
+         */
+        public Builder buildAt(LocalDate builtDate){
+            this.builtDate = builtDate;
+            return this;
+        }
+        /**
+         * Set the number of parks of the property
+         * @param numberOfStoreys of the property
+         * @return the Builder object
+         */
+        public Builder withHowManyStorages(int numberOfStoreys){
+            this.numberOfStoreys = numberOfStoreys;
+            return this;
+        }
+        /**
+         * Set the number of parks of the property
+         * @param nOfStorages of the property
+         * @return the Builder object
+         */
+        public Builder withHowManStorages(int nOfStorages){
+            this.nOfStorages = this.nOfStorages;
+            return this;
+        }
+        /**
+         * Set the number of parks of the property
+         * @param isMultigeneration of the property
+         * @return the Builder object
+         */
+        public Builder withHowManyLockerStorage(boolean isMultigeneration){
+            this.isMultigeneration = isMultigeneration;
+            return this;
+        }
+
+        /**
+         * Finalize the construction of Townhouse using Builder design pattern.
+         * @return Condo using the previously collected information
+         * provided to the Builder object.
+         */
+        public FarmHouse build(){
+            FarmHouse fh = new FarmHouse(this.uuid,this.address,this.price);
+            fh.setType(this.type);
+            fh.setNumberOfStoreys(this.numberOfStoreys);
+            fh.setStorage(this.storageType,nOfStorages);
+            fh.setBuiltDate(this.builtDate);
+            fh.setMultigeneration(this.isMultigeneration);
+            return fh;
+        }
     }
 
-    /**
-     * An abstract method, which cannot be finished here and must be implemented
-     * by non-abstract subclass.
-     *
-     * @param howManyParks the number of parking spaces.
-     */
-    @Override
-    public void setHowManyParks(int howManyParks) {
-
-    }
-
-    /**
-     * return a String using the superClass's toString()
-     * @return String
-     */
-    @Override
-    public String toString() {
-        return "FarmHouse{" + "\n" +
-                super.toString() +
-                "}";
-    }
 }
+
+
