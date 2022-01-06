9bb43cd2127ccf203c1bc9f9234e62c0e9f92298
rui-min <minruiray@outlook.com>
2022-01-06 13:19:54 -0500

    Add back deleted file

---
+++ server_property/src/main/java/mls/server_property/domain/CooperativeHome.java
@@ -0,0 +1,28 @@
+package mls.server_property.domain;
+
+/**
+ * Concrete class representing Cooperative home (level 3)
+ */
+public class CooperativeHome extends Residential {
+
+    /**
+     *Constructor for CooperativeHome obj requiring Long, address and price.
+     * @param uuid Long of the instance.
+     * @param address address of the property.
+     * @param price price of the property.
+     */
+    public CooperativeHome(Long uuid, String address, int price) {
+        super(address, price);
+    }
+
+    /**
+     * Getter for ownership type
+     * @return ownership type
+     */
+    @Override
+    public String getOwnership() {
+        return "Cooperative Home";
+    }
+
+
+}
