11e1be3ea7cda277bb745197ad811c53a43cb0ce
rui-min <minruiray@outlook.com>
2022-01-06 13:15:19 -0500

    modify controller & service a little

--- server_property/src/main/java/mls/server_property/controllers/ResidentialController.java
+++ server_property/src/main/java/mls/server_property/controllers/ResidentialController.java
@@ -1,6 +1,5 @@
 package mls.server_property.controllers;
 
-import mls.server_property.domain.Residential;
 import mls.server_property.domain.Property;
 import mls.server_property.services.ResidentialService;
 import org.springframework.beans.factory.annotation.Autowired;
