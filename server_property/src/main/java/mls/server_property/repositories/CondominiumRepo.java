36f5e188b592f5bbd102e8757cf52745881f3bf9
Wentaohaohhh <77384358+Wentaohaohhh@users.noreply.github.com>
2022-01-07 17:22:00 -0500

    create condominium repo & service & controller draft

---
+++ server_property/src/main/java/mls/server_property/repositories/CondominiumRepo.java
@@ -0,0 +1,18 @@
+package mls.server_property.repositories;
+
+import mls.server_property.domain.Condominium;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.util.Optional;
+@NoRepositoryBean
+@Repository("condominiumRepo")
+public interface CondominiumRepo extends ResidentialRepo{
+    Optional<Condominium> findCondominiumNByOfParkingSpace(int nOfParkingSpace);
+    Optional<Condominium> findCondominiumByStorageType(String storageType);
+    Optional<Condominium> findCondominiumByNOfStorages(int nOfStorages);
+    Optional<Condominium> findCondominiumByBuiltDate(Date builtDate);
+    Optional<Condominium> findCondominiumByEntryDate(Date entryDate);
+
+}
