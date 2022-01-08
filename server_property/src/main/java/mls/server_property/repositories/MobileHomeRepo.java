be1ed27657b6a2d97e8d215adb4bd03254ba6701
Rui Min <70812825+rui-min@users.noreply.github.com>
2022-01-07 22:55:25 -0500

    Delete server_property directory

--- server_property/src/main/java/mls/server_property/repositories/MobileHomeRepo.java
+++
@@ -1,8 +0,0 @@
-package mls.server_property.repositories;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MobileHomeRepo extends ResidentialRepo {
-
-}
--- server_property/src/main/java/mls/server_property/repositories/VacationHomeRepo.java
+++
@@ -1,8 +0,0 @@
-package mls.server_property.repositories;
-
-import mls.server_property.domain.VacationHome;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VacationHomeRepo extends FreeholdRepo<VacationHome> {
-}
