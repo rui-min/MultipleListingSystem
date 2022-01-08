713f6337e213ad9de55218d0255f4a2b35b51a49
Chris2020-wang <69598813+Chris2020-wang@users.noreply.github.com>
2022-01-08 10:17:48 -0500

    Add files via upload

---
+++ front_client/src/main/java/mls/front_client/model/SearchInfo.java
@@ -0,0 +1,40 @@
+package frontend.demo.model;
+
+public class SearchInfo {
+    private String address;
+    private String minPrice;
+    private String maxPrice;
+    private String propertyType;
+
+    public String getPropertyType() {
+        return propertyType;
+    }
+
+    public void setPropertyType(String propertyType) {
+        this.propertyType = propertyType;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMinPrice() {
+        return minPrice;
+    }
+
+    public void setMinPrice(String minPrice) {
+        this.minPrice = minPrice;
+    }
+
+    public String getMaxPrice() {
+        return maxPrice;
+    }
+
+    public void setMaxPrice(String maxPrice) {
+        this.maxPrice = maxPrice;
+    }
+}
