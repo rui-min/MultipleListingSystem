49c71f9e642fc604cd5ef70dcf886cc6a1f92ec9
rui-min <minruiray@outlook.com>
2022-01-09 10:41:49 -0500

    Now main can run

--- server_property/src/main/java/mls/server_property/controllers/CondominiumController.java
+++ server_property/src/main/java/mls/server_property/controllers/CondominiumController.java
@@ -2,13 +2,14 @@ package mls.server_property.controllers;
 
 import mls.server_property.domain.Condominium;
 import mls.server_property.services.CondominiumService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public abstract class CondominiumController extends ResidentialController{
     private CondominiumService condominiumService;
 
-    public CondominiumController(CondominiumService condominiumService) {
+    public CondominiumController(@Qualifier("condoService") CondominiumService condominiumService) {
         super(condominiumService);
     }
 }
