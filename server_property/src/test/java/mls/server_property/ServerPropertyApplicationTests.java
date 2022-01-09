d59191460dddf4528b1876ad0d2ce7f8dfb2d39d
rui-min <minruiray@outlook.com>
2022-01-09 11:07:43 -0500

    !! @MappedSuperclass Bone Version (runnable)

--- server_property/src/test/java/mls/server_property/ServerPropertyApplicationTests.java
+++ server_property/src/test/java/mls/server_property/ServerPropertyApplicationTests.java
@@ -8,19 +8,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes=ServerPropertyApplication.class)
 class ServerPropertyApplicationTests {
-//    @Autowired
-//    LandRepo lr;
-//	@Test
-//	void contextLoads() {
-//        Land vh1 = new Land(1L,"a", 1000000);
-//        Land vh2 = new Land(2L,"b", 1100000);
-//        Land vh3 = new Land(3L,"c", 1100000);
-//        Land vh4 = new Land(4L,"d", 1100000);
-//        lr.save(vh1);
-//        lr.save(vh2);
-//        lr.save(vh3);
-//        lr.save(vh4);
-//        lr.delete(vh2);
-//	}
+    @Autowired
+    LandRepo lr;
+	@Test
+	void contextLoads() {
+        Land lr1 = new Land(1L,"a", 1000000);
+        Land lr2 = new Land(2L,"b", 1100000);
+        Land lr3 = new Land(3L,"c", 1100000);
+        Land lr4 = new Land(4L,"d", 1100000);
+        lr.save(lr1);
+        lr.save(lr2);
+        lr.save(lr3);
+        lr.save(lr4);
+        lr.delete(lr2);
+	}
 
 }
