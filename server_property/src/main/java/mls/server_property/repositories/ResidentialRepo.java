2b80c931ee732a51262734d08e32dd5702e72983
sss <10131931+sowiesss@users.noreply.github.com>
2022-01-10 15:34:02 -0500

    1-10-22 update repo

--- server_property/src/main/java/mls/server_property/repositories/ResidentialRepo.java
+++ server_property/src/main/java/mls/server_property/repositories/ResidentialRepo.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 @Repository
 public interface ResidentialRepo<T extends Residential> extends PropertyRepo<T> {
     Optional<List<T>> findResidentialBynOfParkingSpace(int nOfParkingSpace);
-    Optional<List<T>> findResidentialBystorageType(String storageType);
+    Optional<List<T>> findResidentialByStorageType(String storageType);
     Optional<List<T>> findResidentialBynOfStorages(int nOfStorages);
     Optional<List<T>> findResidentialByBuiltDate(Date builtDate);
     Optional<List<T>> findResidentialByEntryDate(Date entryDate);
