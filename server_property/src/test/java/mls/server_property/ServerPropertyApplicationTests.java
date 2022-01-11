23cae6d664312082f6c10eedd7b5b4c7bc38c569
rui-min <minruiray@outlook.com>
2022-01-10 19:40:39 -0500

    modify test (%...% Not allowed for partialAddress)

--- server_property/src/test/java/mls/server_property/ServerPropertyApplicationTests.java
+++ server_property/src/test/java/mls/server_property/ServerPropertyApplicationTests.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.Optional;
+
 @SpringBootTest(classes=ServerPropertyApplication.class)
 class ServerPropertyApplicationTests {
     @Autowired
@@ -17,21 +20,21 @@ class ServerPropertyApplicationTests {
 
 	@Test
 	void contextLoads() {
-        Land l1 = new Land(1L,"a", 1000000);
-        Land l2 = new Land(2L,"b", 1100000);
-        Land l3 = new Land(3L,"c", 1100000);
-        Land l4 = new Land(4L,"d", 1100000);
-//        VacationHome v1 = new VacationHome(null, "a", 1111111, 0, null, 0, null, null, 0, false);
-//        vhRepo.save(v1);
-        ldRepo.save(l1);
-        ldRepo.save(l2);
-        ldRepo.save(l3);
-        ldRepo.save(l4);
+//        Land l1 = new Land(null,"az", 1000);
+//        Land l2 = new Land(2L,"zb", 1100000);
+//        Land l3 = new Land(3L,"cz", 1100000);
+//        Land l4 = new Land(4L,"zd", 1100000);
+        VacationHome v1 = new VacationHome(null, "a", 1111111, 0, null, 0, null, null, 0, false);
+        vhRepo.save(v1);
+//        ldRepo.save(l1);
+//        ldRepo.save(l2);
+//        ldRepo.save(l3);
+//        ldRepo.save(l4);
 //        long i = vhRepo.count();
 //        long j = ldRepo.count();
 //        long k = 0;
-        System.out.println(ldRepo.findByAddressContainsAndPriceBetween("%a%",100,100000000));
-
+//        Optional<List<Land>> optList= ldRepo.findByAddressContainsAndPriceBetween("z",100,100000000);
+//        System.out.println(optList.get().get(1));
 	}
 
 }
