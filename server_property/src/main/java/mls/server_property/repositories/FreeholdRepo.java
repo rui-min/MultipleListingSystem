b17e9f539a689f72e87484408d79dc0c19a4fecb
sss <10131931+sowiesss@users.noreply.github.com>
2022-01-10 20:26:19 -0500

    1-10-22  modify domain Residential

--- server_property/src/main/java/mls/server_property/repositories/FreeholdRepo.java
+++ server_property/src/main/java/mls/server_property/repositories/FreeholdRepo.java
@@ -14,4 +14,10 @@ public interface FreeholdRepo<T extends Freehold> extends PropertyRepo<T> {
     Optional<List<T>> findByIsMultigenerationTrue();
     Optional<List<T>> findByIsMultigenerationFalse();
     Optional<List<T>> findByNumberOfFloors(int numberOfFloors);
+    Optional<List<T>> findByNumberOfFloorsAndIsMultigenerationTrue(int numberOfFloors);
+    Optional<List<T>> findByNumberOfFloorsAndIsMultigenerationFalse(int numberOfFloors);
+    Optional<List<T>> findByTypeAndIsMultigenerationFalse(String type);
+    Optional<List<T>> findByTypeAndIsMultigenerationTrue(String type);
+    Optional<List<T>> findByNumberOfFloorsAndTypeAndIsMultigenerationTrue(int numberOfFloors, String type);
+    Optional<List<T>> findByNumberOfFloorsAndTypeAndIsMultigenerationFalse(int numberOfFloors, String type);
 }
