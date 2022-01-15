ae66dd0012e29d19a09ab0ef20add881a8746497
rui-min <minruiray@outlook.com>
2022-01-15 11:20:37 -0500

    complete service & controller files

--- server_property/src/main/java/mls/server_property/services/PropertyService.java
+++ server_property/src/main/java/mls/server_property/services/PropertyService.java
@@ -9,46 +9,61 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+@Service("propServ")
 public class PropertyService {
-    // Avoid reporting no bean error for pRepo
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    @Autowired private PropertyRepo<Property> propRepo;
-
-    @Autowired private FreeholdRepo<Freehold> freeholdRepo;
+    @Autowired private PropertyRepo<Property> propertyRepo;
     @Autowired private ResidentialRepo<Residential> residentialRepo;
-    @Autowired private VacationHomeRepo vacationHomeRepo;
-    @Autowired private LandRepo landPropertyRepo;
+    @Autowired private FreeholdRepo<Freehold> freeholdRepo;
+    @Autowired private VacationHomeRepo vacationhomeRepo;
+    @Autowired private LandRepo landRepo;
 //    @Autowired
 
-    public List<Property> getAllProperties(){ return propRepo.findAll(); }
+    public List<Property> getAllProperties(){ return propertyRepo.findAll(); }
 
     public Property getProperties(Long id) throws Throwable {
-        return propRepo.findById(id).orElseThrow(() -> new IllegalStateException(
+        return propertyRepo.findById(id).orElseThrow(() -> new IllegalStateException(
             String.format("No property with id %d exists", id))); }
 
     public List<Property> getProperties(List<Long> ids){
-        return propRepo.findAllById(ids);
+        return propertyRepo.findAllById(ids);
+    }
+
+    public Optional<? extends List<? extends Property>> getProperties(String type, String partialAddress, int lowerBound, int upperBound) {
+        switch (type.toLowerCase()) {
+            case "property":
+                return propertyRepo.findByAddressContainsAndPriceBetween(partialAddress, lowerBound, upperBound);
+                // no need to "break;" because already return
+            case "residential":
+                return residentialRepo.findByAddressContainsAndPriceBetween(partialAddress, lowerBound, upperBound);
+            case "freehold":
+                return freeholdRepo.findByAddressContainsAndPriceBetween(partialAddress, lowerBound, upperBound);
+            case "vacationhome":
+                return vacationhomeRepo.findByAddressContainsAndPriceBetween(partialAddress, lowerBound, upperBound);
+            case "land":
+                return landRepo.findByAddressContainsAndPriceBetween(partialAddress, lowerBound, upperBound);
+        }
+
+        return Optional.empty();    // nothing found
     }
 
     public void addNewProperty(Property property){
-        Optional<Property> propOpt = propRepo.findByAddress(property.getAddress());
+        Optional<Property> propOpt = propertyRepo.findByAddress(property.getAddress());
         if(propOpt.isPresent()){
             throw new IllegalStateException("Property exists at this address");
         }
-        propRepo.save(property);
+        propertyRepo.save(property);
     }
 
     public void removeProperty(Long id){
-        if(! propRepo.existsById(id)){
+        if(! propertyRepo.existsById(id)){
             throw new IllegalStateException(String.format("No property with id %d exists", id));
         }
-        propRepo.deleteById(id);
+        propertyRepo.deleteById(id);
     }
 
     @Transactional
     public void updateProperty(Long id, String address, int price) throws Throwable {
-        Property property = (Property) propRepo.findById(id).orElseThrow(() -> new IllegalStateException(
+        Property property = propertyRepo.findById(id).orElseThrow(() -> new IllegalStateException(
                 String.format("No property with id %d exists", id)));
         if(property.getPrice() != price)
             property.setPrice(price);
