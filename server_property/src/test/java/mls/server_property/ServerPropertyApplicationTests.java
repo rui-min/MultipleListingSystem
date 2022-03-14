63d947e067df8f2ad472cbe97fdba58286a519f5
rui-min <minruiray@outlook.com>
2022-03-14 14:30:21 -0400

    small change

--- server_property/src/test/java/mls/server_property/ServerPropertyApplicationTests.java
+++ server_property/src/test/java/mls/server_property/ServerPropertyApplicationTests.java
@@ -9,52 +9,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest(classes=ServerPropertyApplication.class)
 class ServerPropertyApplicationTests {
-    // Avoid reporting no bean error for pRepo
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    @Autowired
-    PropertyRepo<Property> pRepo;
     @Autowired
     LandRepo ldRepo;
     @Autowired
     VacationHomeRepo vhRepo;
-    @Autowired
-    MultiLexRepo mlRepo;
-    @Autowired
-    StackedTownHouseRepo stRepo;
 
 	@Test
-	void contextLoads() {
-        Land l1 = new Land(null,"ezza", 10000);
-        Land l2 = new Land(null,"b", 110000);
-        VacationHome v1 = new VacationHome(null, "bza", 1111111, 1, null, 0, null, null, 0, false);
-        VacationHome v2 = new VacationHome(null, "edf", 1111, 1, "small", 1, null, null, 3, true);
-        ldRepo.save(l1);
-        ldRepo.save(l2);
-        vhRepo.save(v1);
-        vhRepo.save(v2);
-        MultiLex m1 = new MultiLex(null, "bza", 111111, 1, "small", 1, null, null, 3, true);
-        mlRepo.save(m1);
-        StackedTownHouse st1= new StackedTownHouse(null,"zef",221111,1,null,2,null,4);
-        StackedTownHouse st2= new StackedTownHouse(null,"abc",321111,1,null,2,null,7);
-        StackedTownHouse st3= new StackedTownHouse(null,"ezez",821111,1,null,2,null,4);
-        stRepo.save(st1);
-        stRepo.save(st2);
-        stRepo.save(st3);
-        //below query did not return a unique result: should protect this in service file
-//        System.out.println(vhRepo.findByAddress("a"));
-        System.out.println(vhRepo.count());
-        System.out.println(ldRepo.count());
+	void businessLogicTest() {
+        assertEquals(6,vhRepo.count());
+        assertEquals(6,ldRepo.count());
 
-        System.out.println(pRepo.findAll());
+        Optional<List<Land>> optList1= ldRepo.findByAddressContainsAndPriceBetween("18",100,100000000);
+        assertEquals("Land{id=2, address='zab918', price=100000, buildingType='Land'}",
+                        optList1.get().get(1).toString());
 
-        Optional<List<Land>> optList1= ldRepo.findByAddressContainsAndPriceBetween("z",100,100000000);
-        System.out.println(optList1.get().get(1));
-
-        Optional<List<VacationHome>> optList2= vhRepo.findBynOfParkingSpace(1);
-        System.out.println(optList2.get().get(0));
-
+        Optional<List<VacationHome>> optList2= vhRepo.findBynOfParkingSpace(2);
+        assertEquals("Property{id=14, address='zab936', price=100000, buildingType='VacationHome'}, ownership=null, builtDate=1900-01-01, storageType=above, numberOfStorage=1, numberOfParkingSpace=2, isNewConstruction=false, isHighValue=false, type=recommended, numberOfFloors=null, isMultigeneration=false",
+                        optList2.get().get(0).toString());
 	}
 
 }
